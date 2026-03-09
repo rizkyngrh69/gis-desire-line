5f880bd6aaf85f44f1e1e626d847186c1c392396
rizkyngrh23 <rizkynugraha999@gmail.com>
2026-03-09 12:56:39 +0700

    add favico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Desire Line Mapper",
   description: "GIS web app for visualizing origin–destination desire lines",
+  icons: { icon: "/train.ico" },
 };
 
 export default function RootLayout({
